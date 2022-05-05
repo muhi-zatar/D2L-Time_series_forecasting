730c7b1d7cfea9c4a2def7552bc885a1fd96dc62
muhi-zatar <46324129+muhi-zatar@users.noreply.github.com>
2022-05-05 00:10:20 -0600

    Create RNN.py

---
+++ RNN.py
@@ -0,0 +1,289 @@
+import imp
+import torch
+from torch import nn
+from torch.utils.data import TensorDataset, DataLoader
+import torch.optim as optim
+import numpy as np
+import datetime
+from datetime import date
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn import metrics
+
+
+class GRUModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
+        super(GRUModel, self).__init__()
+
+        self.layer_dim = layer_dim
+        self.hidden_dim = hidden_dim
+
+        self.gru = nn.GRU(
+            input_dim, hidden_dim, layer_dim, batch_first=True, dropout=dropout_prob
+        )
+
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        out, _ = self.gru(x, h0.detach())
+
+
+        out = out[:, -1, :]
+
+        out = self.fc(out)
+
+        return out
+
+class RNNModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
+        super(RNNModel, self).__init__()
+
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+
+        self.rnn = nn.RNN(
+            input_dim, hidden_dim, layer_dim, batch_first=True, dropout=dropout_prob
+        )
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        out, h0 = self.rnn(x, h0.detach())
+
+        # Reshaping the outputs in the shape of (batch_size, seq_length, hidden_size)
+        # so that it can fit into the fully connected layer
+        out = out[:, -1, :]
+
+        # Convert the final state to our desired output shape (batch_size, output_dim)
+        out = self.fc(out)
+        return out
+
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
+        super(LSTMModel, self).__init__()
+
+        # Defining the number of layers and the nodes in each layer
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+
+        # LSTM layers
+        self.lstm = nn.LSTM(
+            input_dim, hidden_dim, layer_dim, batch_first=True, dropout=dropout_prob
+        )
+
+        # Fully connected layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        # Initializing hidden state for first input with zeros
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        # Initializing cell state for first input with zeros
+        c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        # We need to detach as we are doing truncated backpropagation through time (BPTT)
+        # If we don't, we'll backprop all the way to the start even after going through another batch
+        # Forward propagation by passing in the input, hidden state, and cell state into the model
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+
+        # Reshaping the outputs in the shape of (batch_size, seq_length, hidden_size)
+        # so that it can fit into the fully connected layer
+        out = out[:, -1, :]
+
+        # Convert the final state to our desired output shape (batch_size, output_dim)
+        out = self.fc(out)
+
+        return out
+
+class Optimization:
+    def __init__(self, model, loss_fn, optimizer):
+        self.model = model
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
+        self.train_losses = []
+        self.val_losses = []
+    
+    def train_step(self, x, y):
+        # Sets model to train mode
+        self.model.train()
+
+        # Makes predictions
+        yhat = self.model(x)
+
+        # Computes loss
+        loss = self.loss_fn(y, yhat)
+
+        # Computes gradients
+        loss.backward()
+
+        # Updates parameters and zeroes gradients
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+
+        # Returns the loss
+        return loss.item()
+
+    def train(self, train_loader, val_loader, batch_size=64, n_epochs=50, n_features=1):
+        model_path = f'models/{self.model}'
+        device = "cpu"
+        for epoch in range(1, n_epochs + 1):
+            batch_losses = []
+            for x_batch, y_batch in train_loader:
+                x_batch = x_batch.view([batch_size, -1, n_features]).to(device)
+                y_batch = y_batch.to(device)
+                loss = self.train_step(x_batch, y_batch)
+                batch_losses.append(loss)
+            training_loss = np.mean(batch_losses)
+            self.train_losses.append(training_loss)
+
+            with torch.no_grad():
+                batch_val_losses = []
+                for x_val, y_val in val_loader:
+                    x_val = x_val.view([batch_size, -1, n_features]).to(device)
+                    y_val = y_val.to(device)
+                    self.model.eval()
+                    yhat = self.model(x_val)
+                    val_loss = self.loss_fn(y_val, yhat).item()
+                    batch_val_losses.append(val_loss)
+                validation_loss = np.mean(batch_val_losses)
+                self.val_losses.append(validation_loss)
+
+            if (epoch <= 10) | (epoch % 50 == 0):
+                print(
+                    f"[{epoch}/{n_epochs}] Training loss: {training_loss:.4f}\t Validation loss: {validation_loss:.4f}"
+                )
+
+        torch.save(self.model.state_dict(), model_path)
+
+    def evaluate(self, test_loader, batch_size=1, n_features=1):
+        device = "cpu"
+        with torch.no_grad():
+            predictions = []
+            values = []
+            for x_test, y_test in test_loader:
+                x_test = x_test.view([batch_size, -1, n_features]).to(device)
+                y_test = y_test.to(device)
+                self.model.eval()
+                yhat = self.model(x_test)
+                predictions.append(yhat.to(device).detach().numpy())
+                values.append(y_test.to(device).detach().numpy())
+
+        return predictions, values
+
+    def plot_losses(self):
+        plt.plot(self.train_losses, label="Training loss")
+        plt.plot(self.val_losses, label="Validation loss")
+        plt.legend()
+        plt.title("Losses")
+        plt.show()
+        plt.close()
+
+
+def get_model(model, model_params):
+    models = {
+        "rnn": RNNModel,
+        "lstm": LSTMModel,
+        "gru": GRUModel,
+    }
+    return models.get(model.lower())(**model_params)
+
+def prepare_data(train_file, val_file, test_file):
+    train_data = pd.read_csv(train_file, index_col=0)
+    val_data = pd.read_csv(val_file, index_col=0)
+    test_data = pd.read_csv(test_file, index_col=0)
+
+    return train_data, val_data, test_data
+
+batch_size = 64
+
+train_data, val_data, test_data = prepare_data("train_data.csv", "val_data.csv", "test_data.csv")
+#train_norm = (train_data - train_data.mean()) / (train_data.max() - train_data.min())
+#val_norm = (val_data - val_data.mean()) / (val_data.max() - val_data.min())
+#test_norm =  (test_data - test_data.mean()) / (test_data.max() - test_data.min())
+
+features = [col for col in train_data.columns if col != 'SI [MW]']
+
+
+X_train = train_data[features]
+y_train = train_data['SI [MW]']
+X_val = val_data[features]
+y_val = val_data['SI [MW]']
+X_test = test_data[features]
+y_test = test_data['SI [MW]']
+#import pdb;pdb.set_trace()
+ss = StandardScaler()
+X_train_sc = ss.fit_transform(X_train)
+X_val_sc = ss.transform(X_val)
+X_test_sc = ss.transform(X_test)
+
+X1 = train_data.drop('SI [MW]',axis=1)
+y1 = train_data['SI [MW]']
+X2 = val_data.drop('SI [MW]',axis=1)
+y2 = val_data['SI [MW]']
+X3 = test_data.drop('SI [MW]',axis=1)
+y3 = test_data['SI [MW]']
+Xtrain = np.array(X1)
+ytrain  = np.array(y1)
+Xval = np.array(X2)
+yval = np.array(y2)
+Xtest = np.array(X3)
+ytest = np.array(y3)
+Xtrain = torch.Tensor(Xtrain)
+ytrain  = torch.Tensor(ytrain)
+Xval = torch.Tensor(Xval)
+yval = torch.Tensor(yval)
+Xtest = torch.Tensor(Xtest)
+ytest = torch.Tensor(ytest)
+
+train = TensorDataset(Xtrain, ytrain)
+val = TensorDataset(Xval, yval)
+test = TensorDataset(Xtest, ytest)
+#import pdb;pdb.set_trace()
+#test = TensorDataset(test_features, test_targets)
+
+train_loader = DataLoader(train, batch_size=batch_size, shuffle=False, drop_last=True)
+val_loader = DataLoader(val, batch_size=batch_size, shuffle=False, drop_last=True)
+#test_loader = DataLoader(test, batch_size=batch_size, shuffle=False, drop_last=True)
+test_loader_one = DataLoader(test, batch_size=1, shuffle=False, drop_last=True)
+
+input_dim = 21
+output_dim = 1
+hidden_dim = 64
+layer_dim = 1
+batch_size = 64
+dropout = 0
+n_epochs = 100
+learning_rate = 1e-4
+weight_decay = 1e-6
+
+model_params = {'input_dim': input_dim,
+                'hidden_dim' : hidden_dim,
+                'layer_dim' : layer_dim,
+                'output_dim' : output_dim,
+                'dropout_prob' : dropout}
+
+model = get_model('gru', model_params)
+
+loss_fn = nn.MSELoss(reduction="mean")
+optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+
+opt = Optimization(model=model, loss_fn=loss_fn, optimizer=optimizer)
+opt.train(train_loader, val_loader, batch_size=batch_size, n_epochs=n_epochs, n_features=input_dim)
+
+predictions, values = opt.evaluate(test_loader_one, batch_size=1, n_features=input_dim)
+predictions = np.array(predictions)
+values = np.array(values)
+y = predictions.reshape(1,-1)
+yhat = values.reshape(1,-1)
+
+mae = metrics.mean_absolute_error(y, yhat)
+mse = metrics.mean_squared_error(y, yhat)
+
+rmse = np.sqrt(mse) # or mse**(0.5)  
+import pdb;pdb.set_trace()
+#print(mae, mse, rmse)
