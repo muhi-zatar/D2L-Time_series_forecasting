9c5db218693ce3e5697be39ed7d79c268b2e68a1
muhi-zatar <46324129+muhi-zatar@users.noreply.github.com>
2022-05-05 18:55:36 -0600

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -46,7 +46,17 @@ def prepare_data(train_file, val_file, test_file):
 
     return train_data, val_data, test_data
 
+def sliding_windows(data, seq_length):
+    x = []
+    y = []
 
+    for i in range(len(data)-seq_length-1):
+        _x = data[i:(i+seq_length)]
+        _y = data[i+seq_length]
+        x.append(_x)
+        y.append(_y)
+
+    return np.array(x),np.array(y)
 
 batch_size = 64
 
